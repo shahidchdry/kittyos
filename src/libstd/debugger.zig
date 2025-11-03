8cf87049225ef57de5558f433f72a01105c330c0
shahidchdry <shahidchaudhary9@protonmail.com>
2025-11-03 14:18:58 +0000

    updated serial port debugger

--- src/libstd/debugger.zig
+++ src/libstd/debugger.zig
@@ -3,34 +3,32 @@
 const x86 = @import("../libmeow/x86.zig");
 
 // I/O port for COM1
-const SERIAL_PORT =  0x3f8;
-
-// Register offsets from the base port
-const SERIAL_DATA_PORT = SERIAL_PORT + 0;
-const SERIAL_INT_ENABLE_PORT = SERIAL_PORT + 1;
-const SERIAL_FIFO_CONTROL_PORT = SERIAL_PORT + 2;
-const SERIAL_LINE_CONTROL_PORT = SERIAL_PORT + 3;
-const SERIAL_MODEM_CONTROL_PORT = SERIAL_PORT + 4;
-const SERIAL_LINE_STATUS_PORT = SERIAL_PORT + 5;
+const PORT =  0x3f8;
 
 pub fn init() void {
-    x86.out(SERIAL_INT_ENABLE_PORT, @as(u8, 0x00));
-    x86.out(SERIAL_LINE_CONTROL_PORT, @as(u8, 0x80)); 
-    x86.out(SERIAL_DATA_PORT, @as(u8, 0x03)); 
-    x86.out(SERIAL_INT_ENABLE_PORT, @as(u8, 0x00));
-    x86.out(SERIAL_LINE_CONTROL_PORT, @as(u8, 0x03));
-    x86.out(SERIAL_FIFO_CONTROL_PORT, @as(u8, 0xC7));
-    x86.out(SERIAL_MODEM_CONTROL_PORT, @as(u8, 0x0B));
+	x86.out(PORT + 1, @as(u8, 0x00));
+	x86.out(PORT + 3, @as(u8, 0x80));
+    x86.out(PORT + 0, @as(u8, 0x03));
+    x86.out(PORT + 1, @as(u8, 0x00));
+    x86.out(PORT + 3, @as(u8, 0x03));
+    x86.out(PORT + 2, @as(u8, 0xC7));
+    x86.out(PORT + 4, @as(u8, 0x0B));
+    x86.out(PORT + 4, @as(u8, 0x1E));
+    x86.out(PORT + 0, @as(u8, 0xAE));
+
+    if(x86.in(u8, PORT + 0) != 0xAE) {
+       return;
+    }
+    x86.out(PORT + 4, @as(u8, 0x0F));
 }
 
 fn isTransmitEmpty() u8 {
-    return x86.in(u8, SERIAL_LINE_STATUS_PORT) & 0x20;
+    return x86.in(u8, PORT + 5) & 0x20;
 }
 
 fn putChar(char: u8) void {
-    while (isTransmitEmpty() == 0) {
-    	x86.out(SERIAL_DATA_PORT, char);
-    }
+    while (isTransmitEmpty() == 0) {}
+    x86.out(PORT, char);
 }
 
 pub fn log(str: []const u8) void {
